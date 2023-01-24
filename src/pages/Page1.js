96e0f83ac3d5454236925147a8fbe5f203e9ff85
SimonG86 <simon.graham@darwin.com>
2023-01-24 16:28:59 +0000

    added routing

--- src/pages/Page1.js
+++ src/pages/Page1.js
@@ -1,17 +1,15 @@
 import styles from './page.module.css';
 import options from './chartOptions';
 import Card from '../components/UI/Card';
-import ReactEcharts from "echarts-for-react";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 const Page1 = () => {
     return (
         <div className={styles.page}>
             {options.map((option) =>
                 <Card className={styles.container}>
-                    <ReactEcharts
-                        option={option}
-                        style={{ width: "100%", height: "100%" }}
-                    ></ReactEcharts>
+                    <HighchartsReact highcharts={Highcharts} options={option} />
                 </Card>
             )}
         </div>
--- src/pages/Page2.js
+++ src/pages/Page2.js
@@ -1,31 +1,17 @@
 import styles from './page.module.css';
-import options from './chartOptions2';
+import options from './chartOptions1';
 import Card from '../components/UI/Card';
-import ReactEcharts from "echarts-for-react";
+import Highcharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
 
 const Page2 = () => {
     return (
         <div className={styles.page}>
             <Card className={styles.containerLarge}>
-                <ReactEcharts
-                    option={options[0]}
-                    style={{ width: "100%", height: "100%" }}
-                ></ReactEcharts>
-            </Card>
-            <Card className={styles.containerMedium}>
-                <ReactEcharts
-                    option={options[1]}
-                    style={{ width: "100%", height: "100%" }}
-                ></ReactEcharts>
-            </Card>
-            {options.slice(2).map((option) =>
                 <Card className={styles.container}>
-                    <ReactEcharts
-                        option={option}
-                        style={{ width: "100%", height: "100%" }}
-                    ></ReactEcharts>
+                    <HighchartsReact highcharts={Highcharts} options={options[0]} />
                 </Card>
-            )}
+            </Card>
         </div>
     )
 }
